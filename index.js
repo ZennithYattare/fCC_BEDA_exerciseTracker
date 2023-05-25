1a07ccac36f0b4cbf31aa328893b4bca566a2330
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-25 17:16:16 +0800

    connect to mongodb

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+/** @format */
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: "fcc_beda_exercisetracker",
+	})
+	.then(() => {
+		console.log("Connected to the Database.");
+	})
+	.catch((err) => console.error(err));
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+	console.log("Your app is listening on port " + listener.address().port);
+});
