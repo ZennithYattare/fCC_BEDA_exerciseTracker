724ecce67ad922246ac90e17e21a3845e624f1bb
ZennithYattare <luis.espera.0758@gmail.com>
2023-05-25 19:17:04 +0800

    omit unused Log schema

--- index.js
+++ index.js
@@ -29,15 +29,8 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true },
 });
 
-const logSchema = new mongoose.Schema({
-	username: { type: String, required: true },
-	count: { type: Number, required: true },
-	log: [exerciseSchema],
-});
-
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 const User = mongoose.model("User", userSchema);
-const Log = mongoose.model("Log", logSchema);
 
 app.use(cors());
 app.use(express.static("public"));
